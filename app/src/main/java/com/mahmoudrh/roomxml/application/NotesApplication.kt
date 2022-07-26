a80da8d6a41c69bf467b3ea76b5807b5f0173753
MahmoudRH <mhabeeb5@students.iugaza.edu.ps>
2022-07-26 13:58:00 +0300

    added a crash dialog so users can report app crashes via email

--- app/src/main/java/com/mahmoudrh/roomxml/application/NotesApplication.kt
+++ app/src/main/java/com/mahmoudrh/roomxml/application/NotesApplication.kt
@@ -1,10 +1,17 @@
 package com.mahmoudrh.roomxml.application
 
 import android.app.Application
+import android.content.Context
+import com.mahmoudrh.roomxml.BuildConfig
+import com.mahmoudrh.roomxml.R
 import com.mahmoudrh.roomxml.data.NotesDB
 import com.mahmoudrh.roomxml.data.NotesRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
+import org.acra.config.dialog
+import org.acra.config.mailSender
+import org.acra.data.StringFormat
+import org.acra.ktx.initAcra
 
 class NotesApplication: Application() {
 
@@ -12,4 +19,29 @@ class NotesApplication: Application() {
 
     val database by lazy { NotesDB.getInstance(this,applicationScope) }
     val repository by lazy { NotesRepository(database.NoteDAO()) }
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+
+        initAcra {
+            buildConfigClass = BuildConfig::class.java
+            reportFormat = StringFormat.JSON
+
+            dialog {
+                text = getString(R.string.dialog_text)
+                title = getString(R.string.dialog_title)
+                positiveButtonText = getString(R.string.dialog_positive)
+                negativeButtonText = getString(R.string.dialog_negative)
+                commentPrompt = getString(R.string.dialog_comment)
+                resIcon = R.drawable.ic_dialog_icon
+                resTheme = R.style.Theme_Material3_Light_Dialog
+            }
+
+            mailSender {
+                mailTo = "dnote.developer@gmail.com"
+                subject = "D-Note Crash Report"
+                reportFileName = "Crash.txt"
+            }
+        }
+    }
 }
